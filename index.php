dd0718f276f69b5322694ad778ddc8ea9550870e
Mia <55879125+mijaka@users.noreply.github.com>
2021-03-31 18:10:44 +0300

    bd

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+<style>
+    h1{
+        margin:  0 10px;
+    }
+    a{
+        text-align: center;
+        width: 150px;
+        text-decoration: none;
+        display: inline-block;
+        margin:  0 10px;
+        padding: 12px 0;
+        color: black;
+        border: .5px solid black;
+        position: relative;
+        z-index: 1;
+
+    }
+</style>
+</head>
+<body>
+<?php
+Echo
+"<h1>Choose table:</h1>";
+Echo
+"<a href='table1.php'>Table 1</a>";
+Echo
+"<a href='table2.php'>Table 2</a>";
+?>
+</body>
+</html>
